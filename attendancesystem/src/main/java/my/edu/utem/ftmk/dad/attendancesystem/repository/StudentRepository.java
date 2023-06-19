efc0a50a18dba0bd0ca38463d29a66989f034c54
SabrinaAina <116191769+SabrinaAina@users.noreply.github.com>
2023-06-19 11:25:57 +0800

    Update StudentRepository.java
    Add another method findStudentById

--- attendancesystem/src/main/java/my/edu/utem/ftmk/dad/attendancesystem/repository/StudentRepository.java
+++ attendancesystem/src/main/java/my/edu/utem/ftmk/dad/attendancesystem/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package my.edu.utem.ftmk.dad.attendancesystem.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -17,9 +18,12 @@ import my.edu.utem.ftmk.dad.attendancesystem.model.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {	
 	
-	    List<Student> findByStudentName(String studentName);
-	   
-        List<Student> findByStudentNameContaining(String keyword);
-	    
+	List<Student> findByStudentName(String studentName);
+
+	List<Student> findByStudentNameContaining(String keyword);
+	
+	// Find student by Id for absence and attended students
+	Optional<Student> findByStudentId(long studentId);
+
 
 }
