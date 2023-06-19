c2953b7b0975d354bf87e742c03c1f672158f203
SabrinaAina <116191769+SabrinaAina@users.noreply.github.com>
2023-06-19 11:36:26 +0800

    Update RegistrationSubjectRepository.java
    Add two method

--- attendancesystem/src/main/java/my/edu/utem/ftmk/dad/attendancesystem/repository/RegistrationSubjectRepository.java
+++ attendancesystem/src/main/java/my/edu/utem/ftmk/dad/attendancesystem/repository/RegistrationSubjectRepository.java
@@ -1,8 +1,11 @@
 package my.edu.utem.ftmk.dad.attendancesystem.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import my.edu.utem.ftmk.dad.attendancesystem.model.Attendance;
 import my.edu.utem.ftmk.dad.attendancesystem.model.RegistrationSubject;
 
 /**
@@ -14,5 +17,10 @@ import my.edu.utem.ftmk.dad.attendancesystem.model.RegistrationSubject;
  */
 @Repository
 public interface RegistrationSubjectRepository extends JpaRepository<RegistrationSubject, Long> {
-
+  
+   // Retrieve list of student registration subject for the specified subjectId
+	 List<RegistrationSubject> findBySubjectSubjectId(long subjectId);
+  
+   // Calculate total students for a subject 
+	 int countBySubjectSubjectId(long subjectId);
 }
