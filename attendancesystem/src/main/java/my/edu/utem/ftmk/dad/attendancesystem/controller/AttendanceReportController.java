43061931ab2c8db6df6eb3553fd342f0c41e130f
SabrinaAina <116191769+SabrinaAina@users.noreply.github.com>
2023-07-06 20:17:05 +0800

    Update AttendanceReportController.java
    Fetch the attendance through REST

--- attendancesystem/src/main/java/my/edu/utem/ftmk/dad/attendancesystem/controller/AttendanceReportController.java
+++ attendancesystem/src/main/java/my/edu/utem/ftmk/dad/attendancesystem/controller/AttendanceReportController.java
@@ -1,7 +1,7 @@
 package my.edu.utem.ftmk.dad.attendancesystem.controller;
 
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -18,103 +18,143 @@ import my.edu.utem.ftmk.dad.attendancesystem.model.Attendance;
 import my.edu.utem.ftmk.dad.attendancesystem.model.Examination;
 import my.edu.utem.ftmk.dad.attendancesystem.model.RegistrationSubject;
 import my.edu.utem.ftmk.dad.attendancesystem.model.Student;
-import my.edu.utem.ftmk.dad.attendancesystem.repository.AttendanceRepository;
-import my.edu.utem.ftmk.dad.attendancesystem.repository.ExaminationRepository;
-import my.edu.utem.ftmk.dad.attendancesystem.repository.RegistrationSubjectRepository;
-import my.edu.utem.ftmk.dad.attendancesystem.repository.StudentRepository;
 
-/**
- * This class will generate attendance report after the examination
- * 
- * @author nursabrinaainadzulkifli
- *
- */
 @Controller
 public class AttendanceReportController {
 
-	private String defaultURI = "http://localhost:8080/attendancesystem/api/attendances";
+    private String attendanceURI = 
+    		"http://localhost:8080/attendancesystem/api/attendances";
+    private String examinationURI = 
+    		"http://localhost:8080/attendancesystem/api/examinations";
+    private String registrationSubjectURI = 
+    		"http://localhost:8080/attendancesystem/api/registrationsubjects";
+    private String studentURI = 
+    		"http://localhost:8080/attendancesystem/api/students";
 
-	@Autowired
-	private AttendanceRepository attendanceRepository;
+    @GetMapping("/attendancereport/{examId}")
+    public String displayReport(@PathVariable("examId") long examId, 
+    		Model model) {
+        // Fetch the examination based on the examId
+        Examination examination = fetchExamination(examId);
+        if (examination != null) {
+            // Get the subject ID from the examination
+            long subjectId = examination.getSubject().getSubjectId();
 
-	@Autowired
-	private ExaminationRepository examinationRepository;
+            // Calculate the total number of students supposed to attend based 
+            // on subject ID
+            int totalStudents = countRegisteredStudents(subjectId);
 
-	@Autowired
-	private RegistrationSubjectRepository registrationSubjectRepository;
+            // Fetch the attendance records for the specified examId
+            List<Attendance> attendanceList = fetchAttendanceList(examId);
 
-	@Autowired
-	private StudentRepository studentRepository;
+            // Calculate the total attendance count
+            int totalAttendance = attendanceList.size();
 
-	@GetMapping("/attendancereport/{examId}")
-	public String displayReport(@PathVariable("examId") long examId, Model model) {
-		
-		// Fetch the examination based on the examId
-		Examination examination = examinationRepository.findById(examId).orElse(null);
+            // Calculate the total absence count
+            int totalAbsence = totalStudents - totalAttendance;
 
-		System.out.println("Subject" + examination.getSubject().getSubjectId());
-		
-		if (examination != null) {
-			// Get the subject ID from the examination
-			int subjectId = examination.getSubject().getSubjectId();
+            // Fetch the absent students
+            List<Student> absenceList = 
+            		fetchAbsentStudents(subjectId, attendanceList);
 
-			// Calculate the total number of students supposed to attend based on subject ID
-			int totalStudents = registrationSubjectRepository.countBySubjectSubjectId(subjectId);
-			System.out.println("Total Attendance" + totalStudents);
-			
-			// Fetch the attendance records for the specified examId by joining Attendance and RegistrationSubject
-			List<Attendance> attendanceList = attendanceRepository.findByExaminationExamId(examId);
+            // Add the data to the model for rendering in the view
+            model.addAttribute("examination", examination);
+            model.addAttribute("totalStudents", totalStudents);
+            model.addAttribute("totalAttendance", totalAttendance);
+            model.addAttribute("totalAbsence", totalAbsence);
+            model.addAttribute("attendanceList", attendanceList);
+            model.addAttribute("absenceList", absenceList);
+        }
 
-			// Calculate the total attendance count
-			int totalAttendance = attendanceList.size();
-			System.out.println("Total Attendance" + totalAttendance);
-			
-			// Calculate the total absence count
-			int totalAbsence = totalStudents - totalAttendance;
-			System.out.println("Total Absence" + totalAbsence);
+        // Return the name of the view to render
+        return "attendancereport";
+    }
 
-			// Create a list to store the student absences
-			List<Student> absenceList = new ArrayList<>();
+    /**
+     * This method retrieve examination by examination Id
+     * @param examId
+     * @return
+     */
+    private Examination fetchExamination(long examId) {
+        String uri = examinationURI + "/" + examId;
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Examination> response = 
+        		restTemplate.getForEntity(uri, Examination.class);
+        return response.getBody();
+    }
 
-			// Fetch the registration subjects for the specified subjectId
-			List<RegistrationSubject> registrationSubjects = registrationSubjectRepository.findBySubjectSubjectId(subjectId);
+    /**
+     * This method retrieve total students for a subject
+     * @param subjectId
+     * @return
+     */
+    private int countRegisteredStudents(long subjectId) {
+        String uri = registrationSubjectURI + "/count/" + subjectId;
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Integer> response = 
+        		restTemplate.getForEntity(uri, Integer.class);
+        return response.getBody();
+    }
 
-			// Find the student IDs in registrationSubjects
-			List<Integer> registeredStudentIds = registrationSubjects.stream()
-					.map(registrationSubject -> registrationSubject.getStudent().getStudentId())
-					.collect(Collectors.toList());
+    /**
+     * This method retrieve the list of student attendances for an examination.
+     * @param examId
+     * @return
+     */
+    private List<Attendance> fetchAttendanceList(long examId) {
+        String uri = attendanceURI + "/attendancesexam/" + examId;
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Attendance[]> response = 
+        		restTemplate.getForEntity(uri, Attendance[].class);
+        Attendance[] attendanceArray = response.getBody();
+        List<Attendance> attendanceList = Arrays.asList(attendanceArray);
+        
+        // Filter the attendance list based on the specified examId
+        attendanceList = attendanceList.stream()
+                .filter(attendance -> attendance.getExamination().getExamId() 
+                		== examId)
+                .collect(Collectors.toList());
+        
+        return attendanceList;
+    }
 
-			// Find the student IDs in attendanceList
-			List<Integer> attendedStudentIds = attendanceList.stream()
-					.map(attendance -> attendance.getStudent().getStudentId())
-					.collect(Collectors.toList());
+    /**
+     * This method retrieve the list of absent students for an examination
+     * @param subjectId
+     * @param attendanceList
+     * @return
+     */
+    private List<Student> fetchAbsentStudents(long subjectId, 
+    		List<Attendance> attendanceList) {
+        String uri = registrationSubjectURI + "?subjectId=" + subjectId;
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<RegistrationSubject[]> response = 
+        		restTemplate.getForEntity(uri, RegistrationSubject[].class);
+        RegistrationSubject[] registrationSubjects = response.getBody();
+        List<Integer> attendedStudentIds = attendanceList.stream()
+                .map(attendance -> attendance.getStudent().getStudentId())
+                .collect(Collectors.toList());
+        List<Student> absenceList = new ArrayList<>();
+        for (RegistrationSubject registrationSubject : registrationSubjects) {
+            int studentId = registrationSubject.getStudent().getStudentId();
+            if (!attendedStudentIds.contains(studentId)) {
+                Student student = fetchStudent(studentId);
+                absenceList.add(student);
+            }
+        }
+        return absenceList;
+    }
 
-			// Find the student IDs that are absent
-			List<Integer> absentStudentIds = registeredStudentIds.stream()
-					.filter(studentId -> !attendedStudentIds.contains(studentId))
-					.collect(Collectors.toList());
-
-			// Fetch the students based on the absentStudentIds
-			for (Integer studentId : absentStudentIds) {
-				Optional<Student> optionalStudent = studentRepository.findByStudentId(studentId);
-				if (optionalStudent.isPresent()) {
-					absenceList.add(optionalStudent.get());
-				}
-			}
-
-			// Add the data to the model for rendering in the view
-			/* model.addAttribute("examination", examination); */
-			model.addAttribute("totalStudents", totalStudents);
-			model.addAttribute("totalAttendance", totalAttendance);
-			model.addAttribute("totalAbsence", totalAbsence);
-			model.addAttribute("attendanceList", attendanceList);
-			model.addAttribute("absenceList", absenceList);
-		}
-
-		// Return the name of the view to render
-		return "attendancereport";
-	}
-
+    /**
+     * This method will retrieve the student by the studentId
+     * @param studentId
+     * @return
+     */
+    private Student fetchStudent(long studentId) {
+        String uri = studentURI + "/" + studentId;
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Student> response = 
+        		restTemplate.getForEntity(uri, Student.class);
+        return response.getBody();
+    }
 }
-
-
